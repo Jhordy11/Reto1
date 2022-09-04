5d4b894657920b19b72a1222d3ca97cfe0658074
Jhordy11 <fjhordy1@gmail.com>
2022-09-04 02:23:55 -0500

    Final del reto1

---
+++ script.js
@@ -0,0 +1,52 @@
+var textoOriginal;
+var textoFinal;
+const letras = ["a","e","i","o","u"]
+const letrasEnc = ["ai","enter","imes","ober","ufat"]
+
+function desencriptar() {
+    textoOriginal = document.getElementById("textoEncriptable").value;
+    textoFinal = textoOriginal.toLowerCase();
+    for(i = 0; i <letras.length; i++) {
+        if(textoFinal.includes(letrasEnc[i])){
+            textoFinal = textoFinal.replaceAll(letrasEnc[i],letras[i]);
+        }
+    }
+    mostrarTexto();
+}
+
+function encriptar() {
+    textoOriginal = document.getElementById("textoEncriptable").value;
+    textoFinal = textoOriginal.toLowerCase();
+    for(i = 0; i <letras.length; i++) {
+        if(textoFinal.includes(letras[i])){
+            textoFinal = textoFinal.replaceAll(letras[i],letrasEnc[i]);
+        }
+    }
+    mostrarTexto();
+}
+
+function copiarTexto(){
+    var content = document.getElementById('textoResultado');
+    content.select();
+    document.execCommand('copy');
+
+}
+
+function mostrarTexto(){
+    if(textoFinal == ""){
+        document.querySelector(".contenedorResultados").innerHTML=""
+        document.querySelector(".contenedorResultados").innerHTML='<img src="imagenes/Muñeco.png" alt="muñeco" class="imagen3"> <p class="aviso">Ningún mensaje fue encontrado</p> <p class="aviso">Ingrese el texto que desea encriptar o desencriptar.</p>'
+        return;
+    }
+    if(document.querySelector(".contenedorResultados").innerHTML == '<textarea id="textoResultado" cols="70" rows="10" class="textArea" readonly></textarea> <input type="submit" onclick="copiarTexto()" class="boton" value="Copiar"/>'){
+        document.getElementById("textoResultado").innerHTML=textoFinal;
+        return;
+    }
+
+    document.querySelector(".contenedorResultados").innerHTML=""
+    document.querySelector(".contenedorResultados").innerHTML= '<textarea id="textoResultado" cols="70" rows="10" class="textArea" readonly></textarea> <input type="submit" onclick="copiarTexto()" class="boton" value="Copiar"/>'
+    document.getElementById("textoResultado").innerHTML=textoFinal;
+    
+    
+}
+

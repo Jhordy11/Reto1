106f195cf322e6347bb107c1efb1f10a0982a8a8
Jhordy11 <fjhordy1@gmail.com>
2022-09-04 11:52:41 -0500

    Solucionando error de encriptado y uso de transform

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var textoOriginal;
 var textoFinal;
+var numero;
+var letra1;
 const letras = ["a","e","i","o","u"]
 const letrasEnc = ["ai","enter","imes","ober","ufat"]
 
@@ -16,21 +18,32 @@ function desencriptar(myvalue) {
 
 function encriptar(myvalue) {
     textoOriginal = document.getElementById("textoEncriptable").value;
-    textoFinal = textoOriginal.toLowerCase();
-    for(i = 0; i <letras.length; i++) {
-        if(textoFinal.includes(letras[i])){
-            textoFinal = textoFinal.replaceAll(letras[i],letrasEnc[i]);
+    textoOriginal = textoOriginal.toLowerCase();
+    textoFinal = "";
+    for(var i = 0; i <textoOriginal.length ; i++) {
+        numero = comparar(textoOriginal[i])
+        if(numero == -1){
+            textoFinal += textoOriginal[i];
+        }else{
+            textoFinal += letrasEnc[numero];
         }
     }
     mostrarTexto();
+}
+
+function comparar(letra){
+    for(i = 0; i <letras.length; i++) {
+        if(letra == letras[i]){
+            return i;
+        }
+    }
+    return -1;
 }
 
 function copiarTexto(){
     var content = document.getElementById('textoResultado');
     content.select();
     navigator.clipboard.writeText(content.value)
-    mensaje.value = "";
-    alert("Texto Copiado")
 }
 
 function mostrarTexto(){
